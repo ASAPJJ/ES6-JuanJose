d9f96430bf83a0b91f08097d8c9ad748b3dae694
ASAPJJ <juanjosebrunelli@gmail.com>
2022-08-29 14:56:39 -0300

    Tarea JS

---
+++ main.js
@@ -0,0 +1,34 @@
+// const saludar = (nombre)=> {
+//     return `Hola ${nombre}`;
+// }
+
+// // Mostrar el resultado por consola
+// console.log(saludar('Alejandro'));
+
+// function multiplicar(a, b){
+//     return console.log(a+b);
+// }
+
+// // Mostrar el resultado por consola
+// multiplicar(3, 15)
+
+// function crearAlumno(nombre, apellido, edad, materias){
+//     let alumno = {
+//         nombre,
+//         apellido,
+//         edad,
+//         materias
+//     }
+
+//     return console.log(alumno);
+// }
+
+// // Mostrar el resultado por consola.
+// crearAlumno('Leo', 'Messi', '32', ['TLPA','Base de Datos','Inglés'])
+
+const devolverFecha = (dia, mes, año)=>{
+    return console.log(`Hoy es ${dia}/${mes}/${año}`);
+}
+
+// Mostrar el resultado por consola.
+devolverFecha('10','09','2022');
