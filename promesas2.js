bf15fd25abb892354aa4b75f053268b2ee827350
ASAPJJ <juanjosebrunelli@gmail.com>
2022-09-05 21:18:27 -0300

    promesas2 con fetch funcional

--- promesas2.js
+++ promesas2.js
@@ -1,34 +1,50 @@
 const retrasar = milisegundos => new Promise(resolve => setTimeout(resolve, milisegundos));
 
 // Función que retorna los datos de provincias
-function obtenerPcias() {
-    await retrasar(1800);
+async function obtenerPcias() {
+    try {
+        await retrasar(1800);
 
-    const consulta = fetch('');
+        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/provincias');
+        rtajson = consulta.json();
+        return rtajson;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // Función que retorna los datos de departamentos
-function obtenerDptos() {
-    await retrasar(1391);
-
-    const consulta = fetch('');
+async function obtenerDptos() {
+    try {
+        await retrasar(1391);
+        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/departamentos');
+        const rtajson = consulta.json();
+        return rtajson;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // Función que retorna los datos de localidades
-function obtenerLocalidades() {
-    await retrasar(900);
-
-    const consulta = fetch('');
+async function obtenerLocalidades() {
+    try {
+        await retrasar(900);
+        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/localidades');
+        const rtajson = consulta.json();
+        return rtajson;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
 
 
 // Funcion para obtener todos los datos
-function consultarDatos() {
-    const provincias = obtenerPcias();
-    const dptos = obtenerDptos();
-    const localidades = obtenerLocalidades();
+const consultarDatos = async () => {
+    const provincias = await obtenerPcias();
+    const dptos = await obtenerDptos();
+    const localidades = await obtenerLocalidades();
 
     console.log(provincias);
     console.log(dptos);
@@ -36,4 +52,3 @@ function consultarDatos() {
 }
 
 consultarDatos();
-
